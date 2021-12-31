d0b2923ddeca73a414df94dfdd17a5bce966a080
AdithyaM <Aadithyamba@gmail.com>
2021-12-31 17:49:22 +0530

    Username Functionality

---
+++ target/classes/org/make/Tabe.java
@@ -0,0 +1,10 @@
+package org.make;
+
+public class Tabe {
+	private void password() {
+System.out.println("Password");
+	}
+public static void main(String[] args) {
+	
+}
+}
---
+++ target/classes/org/xmas/Sam.java
@@ -0,0 +1,10 @@
+package org.xmas;
+
+public class Sam {
+	private void username() {
+		System.out.println("Username");
+	}
+public static void main(String[] args) {
+	
+}
+}
